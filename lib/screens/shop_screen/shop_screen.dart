350e3bf92a4d88afd1c84fe7ca67c961fc02968b
MikaKuhlmann <mika.kuhlmann@roche.com>
2022-06-11 10:54:34 +0200

    Commit

--- lib/screens/shop_screen/shop_screen.dart
+++ lib/screens/shop_screen/shop_screen.dart
@@ -24,9 +24,9 @@ class ShopScreen extends StatelessWidget {
           padding: const EdgeInsets.only(top: 20, bottom: 10),
           child: Column(
             children: [
-              SvgPicture.asset('assets/icons/carrot.svg'),
+              SvgPicture.asset('assets/icons/DHBW-Logo.svg'),
               SizedBox(height: 5),
-              Text('Istanbul, Levent'),
+              Text('Bestellapp DHBW'),
               SizedBox(height: 10),
               SearchField(),
               SizedBox(height: 10),
